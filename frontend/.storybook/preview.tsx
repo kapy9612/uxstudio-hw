a19caa2d3c0d573edb053a1144b32bfab94211e2
Káposzta Gergő <gergo.kaposzta@kifli.tech>
2023-10-07 23:04:38 +0200

    Storybook fix

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -1,18 +1,22 @@
 import '/src/styles/fonts.css';
 import type { Preview } from '@storybook/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { GlobalStyle } from '../src/styles/global';
+
+const queryClient = new QueryClient();
 
 const preview: Preview = {
     decorators: [
         (Story) => (
-            <>
+            <QueryClientProvider client={queryClient}>
                 <GlobalStyle />
                 <Story />
-            </>
+            </QueryClientProvider>
         ),
     ],
     parameters: {
+        backgrounds: { default: 'dark' },
         actions: { argTypesRegex: '^on[A-Z].*' },
         controls: {
             matchers: {
